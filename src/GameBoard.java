fc97b7f55ff916cc39ac99e48b43209374b1de2d
Plamen Georgiev <pg_97@abv.bg>
2017-11-02 18:52:15 +0200

    final brushes

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -1,23 +1,18 @@
 
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.*;
 import java.util.concurrent.*;
-import javax.imageio.ImageIO;
 import javax.swing.*;
 
 
 @SuppressWarnings("serial")
 public class GameBoard extends JFrame {
-	public static int boardWidth = 1000;
-	public static int boardHeight = 1000;
+	public static int boardWidth = 800;
+	public static int boardHeight = 600;
 	public static int keyHeld=0;
 	public static int  pressedkeyCode = 0;
 	public static boolean running=true;
@@ -65,15 +60,9 @@ public class GameBoard extends JFrame {
 				
 				
 			}
-
-	
-
 			@Override
 			public void keyTyped(KeyEvent arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-			
+			}			
 		});
 			gamePanel = new GameDrawingPanel();
 			gamePanel.setLayout(null);
@@ -157,147 +146,8 @@ public class GameBoard extends JFrame {
 			
 		} catch (FileNotFoundException e) {
 		} catch (Throwable e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 }
 
-class RepaintTheBoard implements Runnable{
-
-	GameBoard brd;
-	RepaintTheBoard(GameBoard brdparam)
-	{
-		brd=brdparam;
-	}
-	public void run() {
-			if(brd.check())
-			{
-				brd.repaint();	
-			}
-			else
-			{
-				brd.paintEndScreen();	
-				return;
-			}
-			
-	}
-	
-}
-class GameDrawingPanel extends JComponent
-{
-	public ArrayList<Cop> cops= new ArrayList<Cop>();
-	public Car car = new Car((GameBoard.boardWidth-Car.carWidth)/2);
-	int width = GameBoard.boardWidth;
-	int height = GameBoard.boardHeight;
-	Clock cl=new Clock();
-	
-	public GameDrawingPanel()
-	{
-		Random rand = new Random();
-		for(int i=0;i<GameBoard.boardWidth/100;i++)
-		{
-			int startingX = (rand.nextInt(10) + 1)*GameBoard.boardWidth/10;
-			if(startingX<200)
-			{
-				cops.add(new Cop(startingX,100));
-			}
-			else
-			{
-				cops.add(new Cop(startingX,5));
-			}
-			
-		}
-		
-		cl.start();
-	}
-	public long getCurrentTime()
-	{
-		return cl.getElapsedTimeSecs();
-	}
-	public boolean intersect()
-	{
-		for(Cop c : cops)
-		{
-			if(c.intersects(car))
-			{
-				return true;
-			}
-		}
-		return false;
-	}
-	synchronized public void paint(Graphics g) 
-	{
-		Graphics2D graphicSettings =(Graphics2D)g;
-		graphicSettings.setColor(Color.PINK);
-		graphicSettings.fillRect(0,0,getWidth(),getHeight());
-		graphicSettings.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
-		graphicSettings.setPaint(Color.BLACK);
-		String time = String.valueOf(cl.getElapsedTimeSecs());
-		g.setFont(new Font("TimesRoman", Font.PLAIN, 100));
-		graphicSettings.drawString(time, 100, 100);
-		int decide=0;
-		Random rand = new Random();
-		decide = rand.nextInt(100);
-	
-		if(decide<10)
-		{
-			int startingX = (rand.nextInt(10) + 1)*GameBoard.boardWidth/10;
-			if(startingX<200)
-			{
-				cops.add(new Cop(startingX,100));
-			}
-			else
-			{
-				cops.add(new Cop(startingX,5));
-			}
-			
-		}
-		BufferedImage bi=null;
-		try {
-		bi = ImageIO.read(new File("C:\\Users\\c5262015\\eclipse-workspace\\TheGame\\rsz_1pc.png"));
-		} catch (IOException e) {
-		}
-		for(Cop c : cops)
-		{
-			c.move();
-			graphicSettings.drawImage(bi,c.x, c.y, null);
-		}
-		for(int i=0; i<cops.size(); i++)
-		{	
-			Cop temp = cops.get(i);
-			if(temp.y>=GameBoard.boardHeight)
-				cops.remove(temp);
-		}
-		car.move(GameBoard.pressedkeyCode,GameBoard.keyHeld);
-		BufferedImage biCar=null;
-		try {
-		biCar = ImageIO.read(new File("C:\\Users\\c5262015\\eclipse-workspace\\TheGame\\rsz_test.png"));
-		} catch (IOException e) {
-		}
-	graphicSettings.drawImage(biCar,car.x, car.y, null);
-	/*
-		graphicSettings.setColor(Color.BLUE);
-		graphicSettings.fill(car);
-		graphicSettings.draw(car);
-		*/
-		}
-	}
-class ListenForButton implements ActionListener
-{
-	public boolean flag;
-	public void actionPerformed(ActionEvent act) {
-		new GameBoard();
-		flag=true;
-	}
-}
-class ListenForButton2 implements ActionListener
-{
-
-	@Override
-	public void actionPerformed(ActionEvent act) {
-		System.exit(0);
-		
-	}
-	
-}
